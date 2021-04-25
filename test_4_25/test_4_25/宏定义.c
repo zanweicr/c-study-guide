50cd5f524ab3d2663b8705ffc43d63caf1edd5f4
zanweicr <79506551+zanweicr@users.noreply.github.com>
2021-04-25 20:58:55 +0800

    Add files via upload

---
+++ test_4_25/test_4_25/宏定义.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+//
+//
+////函数
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+////宏
+//#define MAX(X,Y) (X>Y?X:Y)
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = 0;
+//	//函数
+//	max = Max(a, b);
+//	printf("max = %d ",max);
+//	//宏的方式
+//	max = MAX(a, b);
+//	printf("max = %d ", max);
+//
+//	return 0;
+//}
