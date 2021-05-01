384701ff1cbb9b711f252fa7d83ffe9f4e7fac84
zanweicr <79506551+zanweicr@users.noreply.github.com>
2021-05-01 10:32:40 +0800

    Add files via upload

---
+++ test_4_30/test_4_30/for circulation example 5.c
@@ -0,0 +1,28 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = {0};
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("请输入密码->");
+//		scanf("%s", &password);
+//		if (strcmp(password, "123456") == 0)
+//		{
+//			printf("密码正确\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("密码错误,请再次尝试\n");
+//		}
+//	}
+//	if (i == 3)
+//	{
+//		printf("三次均错误,账户已锁\n"); 
+//	}
+//	return 0;
+//}
